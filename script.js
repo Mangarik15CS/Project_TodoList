6f17f86b986376780e8d2fe0cf31807ad3865c25
I Nyoman Ari Yuda <138686281+Mangarik15CS@users.noreply.github.com>
2023-07-06 07:56:46 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+//Deklarasi Variabel
+
+let listName = document.getElementById("list-name");
+let btnSave = document.getElementById("btn-save");
+let list_item = [];
+let listHTML = "";
+
+// Menyimpan Data Atau Item
+btnSave.addEventListener("click", function () {
+  if (listName.value === "") {
+    alert("Nama Harus diisi! Tidak Boleh Kosong!!!");
+  } else {
+    let listContainer = document.querySelector(".list-group");
+    let listItemHTML = `<li class="list-group-item d-flex justify-content-between">
+                        <span>${listName.value}</span>
+                        <button class="badge border-0 bg-danger btn-hapus">X</button></li>`;
+
+    listHTML += listItemHTML;
+
+    // Menambahkan Item Baru Ke Local Storage
+    manageLocalStorage("TAMBAH", listName.value);
+
+    listContainer.innerHTML = listHTML;
+    listName.value = "";
+    listName.focus();
+
+    // Menghapus Item
+    let btnHapus = document.querySelectorAll(".btn-hapus");
+    for (let x = 0; x < btnHapus.length; x++) {
+      let hapus = btnHapus[x];
+      hapus.addEventListener("click", function () {
+        let itemText = hapus.parentElement.querySelector("span").textContent;
+        hapus.parentElement.remove();
+        manageLocalStorage("HAPUS", itemText); // Menghapus data berdasarkan teks item
+      });
+    }
+  }
+});
+
+// Local Storage
+
+if (localStorage.getItem("TO_DO_ITEMS")) {
+  let itemLocalStorage = JSON.parse(localStorage.getItem("TO_DO_ITEMS"));
+
+  itemLocalStorage.forEach(function (itemTodo) {
+    let listItemHTML = `<li class="list-group-item d-flex justify-content-between">
+                        <span>${itemTodo}</span>
+                        <button class="badge border-0 bg-danger btn-hapus">X</button></li>`;
+
+    listHTML += listItemHTML;
+
+    let listContainer = document.querySelector(".list-group");
+    listContainer.innerHTML = listHTML;
+  });
+}
+
+function manageLocalStorage(action, item) {
+  switch (action) {
+    case "TAMBAH":
+      list_item.push(item);
+      break;
+    case "HAPUS":
+      list_item = list_item.filter(function (todoItem) {
+        return todoItem !== item;
+      });
+      break;
+  }
+
+  localStorage.setItem("TO_DO_ITEMS", JSON.stringify(list_item));
+}
+
+// Kode Untuk Memperbarui Daftar Item Setelah Halaman Dimuat
+window.addEventListener("load", function () {
+  list_item = JSON.parse(localStorage.getItem("TO_DO_ITEMS")) || [];
+  listHTML = "";
+
+  list_item.forEach(function (itemTodo) {
+    let listItemHTML = `<li class="list-group-item d-flex justify-content-between">
+                        <span>${itemTodo}</span>
+                        <button class="badge border-0 bg-danger btn-hapus">X</button></li>`;
+
+    listHTML += listItemHTML;
+  });
+
+  let listContainer = document.querySelector(".list-group");
+  listContainer.innerHTML = listHTML;
+
+  let btnHapus = document.querySelectorAll(".btn-hapus");
+  for (let x = 0; x < btnHapus.length; x++) {
+    let hapus = btnHapus[x];
+    hapus.addEventListener("click", function () {
+      let itemText = hapus.parentElement.querySelector("span").textContent;
+      hapus.parentElement.remove();
+      manageLocalStorage("HAPUS", itemText); // Menghapus Data Berdasarkan Teks Item
+    });
+  }
+});
